2ed1050b3bfdcd7daeee00652e11bb324d1b6d10
soncole <sons1998@naver.com>
2023-11-19 22:09:14 +0900

    useLocation 이용하여 검색 페이지 구현

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,13 @@
 import React, {useState, useEffect} from 'react'
 import "./Nav.css"
+import { useNavigate } from 'react-router-dom';
 export default function Nav() {
 
     const [show, setShow] = useState(false);
 
+    const [searchValue, setSearchValue] = useState("");
+
+    const navigate = useNavigate();
     useEffect(() => {
       window.addEventListener("scroll", () => {
         console.log(window.scrollY);
@@ -19,6 +23,10 @@ export default function Nav() {
       }
     }, []);
     
+    const handleChange = (e) => {
+      setSearchValue(e.target.value);
+      navigate(`/search?q=${e.target.value}`);
+    };
 
   return (
     <nav className={`nav ${show && "nav_black"}`}>
@@ -28,6 +36,9 @@ export default function Nav() {
         className='nav_logo'
         onClick={() => window.location.reload()}
         />
+
+        <input value={searchValue} onChange={handleChange} className='nav_input' type='text' placeholder='영화를 검색해주세요.' />
+
         <img
         alt="profile"
         src="https://occ-0-4796-988.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABbme8JMz4rEKFJhtzpOKWFJ_6qX-0y5wwWyYvBhWS0VKFLa289dZ5zvRBggmFVWVPL2AAYE8xevD4jjLZjWumNo.png?r=a41"
--- src/pages/searchpage/index.js
+++ src/pages/searchpage/index.js
@@ -1,6 +1,34 @@
-import React from 'react'
+import axios from '../../api/axios';
+import React, { useState, useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
 
 export default function SearchPage() {
+  
+  const [searchResults, setSearchResults] = useState([]);
+  
+  const useQuery = () => {
+    return new URLSearchParams(useLocation().search);
+  };
+  
+  let query = useQuery();
+  const searchTerm = query.get("q");
+  
+  useEffect(() => {
+    if(searchTerm) {
+      fetchSearchMovie(searchTerm);
+    }
+  }, [searchTerm]);
+
+  const fetchSearchMovie = async (searchTerm) => {
+    try {
+      const request = await axios.get(`search/multi?include_adult=true&query=${searchTerm}`);
+      setSearchResults(request.data.results);
+    } catch(e) {
+      console.log("error: ", e);
+    }
+  }
+  
+
   return (
     <div>SearchPage</div>
   )
