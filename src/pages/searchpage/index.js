302df4916b1574ba875d1a0d53dc66fc1602532a
soncole <sons1998@naver.com>
2023-11-25 23:24:18 +0900

    useDebounce 커스텀 훅  만들고 적용하기

--- src/pages/searchpage/index.js
+++ src/pages/searchpage/index.js
@@ -2,6 +2,7 @@ import axios from '../../api/axios';
 import React, { useState, useEffect } from 'react'
 import { useLocation } from 'react-router-dom'
 import "./searchpage.css";
+import { useDebounce } from '../../hooks/useDebounce';
 
 export default function SearchPage() {
   
@@ -13,12 +14,13 @@ export default function SearchPage() {
   
   let query = useQuery();
   const searchTerm = query.get("q");
+  const debouncedSearchTerm = useDebounce(searchTerm, 500);
 
   useEffect(() => {
-    if(searchTerm) {
-      fetchSearchMovie(searchTerm);
+    if(debouncedSearchTerm) {
+      fetchSearchMovie(debouncedSearchTerm);
     }
-  }, [searchTerm]);
+  }, [debouncedSearchTerm]);
 
   const fetchSearchMovie = async (searchTerm) => {
     try {
@@ -36,7 +38,7 @@ export default function SearchPage() {
           if(movie.backdrop_path !== null && movie.media_type !== "person") {
             const movieImageUrl = "https://image.tmdb.org/t/p/w500" + movie.backdrop_path;
             return (
-              <div className='movie'>
+              <div className='movie' key={movie.id}>
                 <div className='movie_column-poster'>
                   <img src={movieImageUrl} alt="movie img" className='movie_poster' />
                 </div>
@@ -48,7 +50,7 @@ export default function SearchPage() {
     ) : (
       <section className='no-results'>
         <div className='no-results_text'>
-          <p>찾고자하는 검색어"{searchTerm}"에 맞는 영화가 없습니다.</p>
+          <p>찾고자하는 검색어"{debouncedSearchTerm}"에 맞는 영화가 없습니다.</p>
         </div>
       </section>
     )
